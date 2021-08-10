5e71c350e92075e192c2d78fff3faaa160ddd525
srini <kgs.sri@gmail.com>
2021-08-10 07:43:46 +0530

    update the namenode address

--- src/main/scala/retail/driver/runretail.scala
+++ src/main/scala/retail/driver/runretail.scala
@@ -28,9 +28,9 @@ object runretail {
       logger.warn("======process started at " + format.format(starttime))
       
       val spark = SparkSession.builder()
-      .master("local")
-      .config("hive.metastore.uris","thrift://104.154.223.71:9083")
-       .config("spark.sql.warehouse.dir", "/user/hive/warehouse")
+      //.master("local")
+      .config("hive.metastore.uris","thrift://35.232.181.37:9083")
+      .config("spark.sql.warehouse.dir", "/user/hive/warehouse")
       .appName("Retail-coreengine")
       .config("spark.sql.debug.maxToStringFields", 1000)
       .enableHiveSupport()
